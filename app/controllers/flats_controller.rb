5a396c76efe6c2a39142a6f035e5bf47e5624949
Rzt Prog <rztprog@outlook.fr>
2022-04-27 15:56:01 +0200

    Add show and create method in controller and routes

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,4 +7,24 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_nigth, :number_of_guests)
+  end
 end
